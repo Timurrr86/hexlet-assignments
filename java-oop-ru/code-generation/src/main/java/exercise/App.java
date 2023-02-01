d5a543805e9c1add17355f61b951c2882388fec0
@hexlet/cli <support@hexlet.io>
2023-02-01 23:01:34 +0500

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/App.java
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -1,9 +1,21 @@
 package exercise;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 
 // BEGIN
+public class App {
+    public static void save(Path path, Car car) throws IOException {
+        String data = car.serialize();
+        Files.writeString(path, data, StandardOpenOption.CREATE);
+    }
+
+    public static Car extract(Path path) throws IOException {
+        String jsonRepresentation = Files.readString(path);
+        return Car.unserialize(jsonRepresentation);
+    }
+}
 
 // END
--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -3,8 +3,10 @@ package exercise;
 import lombok.Value;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-// BEGIN
+import java.io.IOException;
 
+// BEGIN
+@Value
 // END
 class Car {
     int id;
@@ -14,6 +16,14 @@ class Car {
     User owner;
 
     // BEGIN
-    
+    public String serialize() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(this);
+    }
+
+    public static Car unserialize(String jsonRepresentation) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonRepresentation, Car.class);
+    }
     // END
 }
