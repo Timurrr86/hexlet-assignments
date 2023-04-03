f10362015ad7fec70a4a1517573425a4f1ff4658
@hexlet/cli <support@hexlet.io>
2023-04-03 23:39:18 +0500

    submit java-web-ru/sql

--- java-web-ru/sql/solution.sql
+++ java-web-ru/sql/solution.sql
@@ -1,1 +1,1 @@
-select first_name, birthday from users where birthday > '1999-10-22' order by first_name limit 3;
+select first_name, birthday from users where birthday > '1999-10-23' order by first_name limit 3;
