b63d8be0cd9cddd5dd71ac8c729ff33f759b2a0a
@hexlet/cli <support@hexlet.io>
2023-12-17 23:35:56 +0500

    submit java-spring-ru/dto-to-entity-for-update

--- java-spring-ru/dto-to-entity-for-update/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/dto-to-entity-for-update/src/main/java/exercise/controller/ProductsController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.http.HttpStatus;
@@ -45,7 +44,7 @@ public class ProductsController {
     }
 
     @GetMapping(path = "/{id}")
-    @ResponseStatus(HttpStatus.UPD)
+    @ResponseStatus(HttpStatus.CREATED)
     public ProductDTO create(@RequestBody ProductCreateDTO productData) {
         var product = toEntity(productData);
         productRepository.save(product);
