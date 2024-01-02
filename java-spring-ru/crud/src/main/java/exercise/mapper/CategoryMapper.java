f74001176d7ed9ac03e3e405ffe32b95a8059f68
@hexlet/cli <support@hexlet.io>
2024-01-02 22:49:46 +0500

    submit java-spring-ru/crud

--- java-spring-ru/crud/src/main/java/exercise/mapper/CategoryMapper.java
+++ java-spring-ru/crud/src/main/java/exercise/mapper/CategoryMapper.java
@@ -3,7 +3,6 @@ package exercise.mapper;
 import exercise.dto.CategoryCreateDTO;
 import exercise.dto.CategoryDTO;
 import exercise.model.Category;
-import org.mapstruct.Mapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.NullValuePropertyMappingStrategy;
@@ -18,10 +17,7 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class CategoryMapper {
-    @Mapping(target = "author", source = "authorId")
     public abstract Category map(CategoryCreateDTO dto);
-
-    @Mapping(source = "author.id", target = "authorId")
     public abstract CategoryDTO map(Category model);
 
 }
--- java-spring-ru/crud/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-ru/crud/src/main/java/exercise/mapper/ProductMapper.java
@@ -19,12 +19,14 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class ProductMapper {
-    @Mapping(target = "author", source = "authorId")
+    @Mapping(target = "category", source = "categoryId")
     public abstract Product map(ProductCreateDTO dto);
 
-    @Mapping(source = "author.id", target = "authorId")
+    @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "category.name", target = "categoryName")
     public abstract ProductDTO map(Product model);
 
+    @Mapping(target = "category", source = "categoryId")
     public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
 }
 // END
