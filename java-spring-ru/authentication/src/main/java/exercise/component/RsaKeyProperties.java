3e411e8ae13e09221417df1d68bcfdcb1cd811d7
@hexlet/cli <support@hexlet.io>
2024-01-05 23:42:28 +0500

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/java/exercise/component/RsaKeyProperties.java
@@ -0,0 +1,38 @@
+package exercise.component;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Component
+@ConfigurationProperties(prefix = "rsa")
+@Setter
+@Getter
+public class RsaKeyProperties {
+    private RSAPublicKey publicKey;
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    private RSAPrivateKey privateKey;
+}
+// END
